f05aa5b9ded58ec6057f07361f254a6d0dcf135e
ReeceBDev <reece.bowen99@gmail.com>
2026-02-12 02:24:35 +0000

    Added capacitor. - modified vite config to allow network access - Added android and ios to capacitor. - modified gitignore.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.arcanaengine.client',
+  appName: 'Arcana Engine',
+  webDir: 'dist'
+};
+
+export default config;
